69f94a8316582840f5996a3c599e618baa22a697
Aaron Sky <aaronsky@skyaaron.com>
2020-07-03 19:27:45 -0400

    Add bk token secret to GHA

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -13,7 +13,7 @@ export MACOS_SOURCES_PATH="$SOURCES_PATH/macOS"
 # Create env.swift
 env_file="$SHARED_SOURCES_PATH/env.swift"
 cp "$SHARED_SOURCES_PATH/env.swift.example" "$env_file"
-if [ -n "${SECRET_BUILDKITE_TOKEN}" ]; then
+if [ -n "${SECRET_BUILDKITE_TOKEN:-}" ]; then
     sed -i "" -E 's/\{\{ SECRET_BUILDKITE_TOKEN \}\}/'"${SECRET_BUILDKITE_TOKEN}"'/' "$env_file"
 else
     echo "*** SECRET_BUILDKITE_TOKEN was not defined. Please enter it yourself into $env_file"
